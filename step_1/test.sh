a6df12c53561dc9a141a3dadfec569c06178affc
Noel Kelly <noel@noelkelly.biz>
2015-12-31 12:49:38 +1000

    genaralise step_1/test.rb for generic domains

--- step_1/test.sh
+++ step_1/test.sh
@@ -1,10 +1,10 @@
-docker build -t noel/ff ff
+docker build -t noel/domain
 
-docker run -d -p  8080:80 noel/ff
+docker run -d -p  8080:80 noel/domain
 
-docker build -t noel/photonscoop photonscoop
+docker build -t noel/photonscoop example
 
-docker run -d -p  8081:80 noel/photonscoop
+docker run -d -p  8081:80 noel/example
 
 docker build -t noel/proxy proxy
 
@@ -13,5 +13,5 @@ docker run -d -p  80:80 noel/proxy
 curl "http://carrock:8080/"
 curl "http://carrock:8081/"
 
-curl "http://ff.photonscoop.local"
-curl "http://photonscoop.local"
+curl "http://domain.local"
+curl "http://example.local"
